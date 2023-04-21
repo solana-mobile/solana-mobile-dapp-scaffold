0b46b48863bd952cfd8b97e75891e0c8bf32811e
Taylor123 <nftaylor11@gmail.com>
2023-04-21 13:57:43 -0700

    fix dependency removal

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import 'react-native-get-random-values';
-import 'react-native-url-polyfill/auto';
 
 AppRegistry.registerComponent(appName, () => App);
