572c74c00b11bd45a0f2c6737853ec21a16db1ba
michael sulistio <michaelsulistio@gmail.com>
2023-05-05 08:56:30 -0700

    Fix crypto and buffer polyfill

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /**
  * @format
  */
+import {Buffer} from 'buffer';
+import 'react-native-get-random-values';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-import {Buffer} from 'buffer';
-import 'react-native-get-random-values';
 
 // Mock event listener functions to prevent them from fataling.
 window.addEventListener = () => {};
